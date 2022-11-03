41331bcf7322f081c7088168379c22026c17e0f0
Mr-DD29 <59406169+Mr-DD29@users.noreply.github.com>
2022-11-03 19:18:03 +0530

    Add files via upload

---
+++ Spring Setter Injection/src/com/setter/injection/Employee.java
@@ -0,0 +1,61 @@
+package com.setter.injection;
+
+public class Employee {
+	
+	private String name;
+	private String mail;
+	private int salary;
+	private Address address;
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String name, String mail, int salary, Address address) {
+		super();
+		this.name = name;
+		this.mail = mail;
+		this.salary = salary;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", mail=" + mail + ", salary=" + salary + ", address=" + address + "]";
+	}
+	
+
+}
