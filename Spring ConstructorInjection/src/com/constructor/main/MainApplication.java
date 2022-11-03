bea03739494b10f4681c189c20793606cc2dc7e2
Mr-DD29 <59406169+Mr-DD29@users.noreply.github.com>
2022-11-03 19:16:23 +0530

    Add files via upload

---
+++ Spring ConstructorInjection/src/com/constructor/main/MainApplication.java
@@ -0,0 +1,35 @@
+package com.constructor.main;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.constructor.injection.Employee;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		
+		Resource xmlResource = new ClassPathResource("/Spring.xml");
+		BeanFactory beanFactory = new XmlBeanFactory(xmlResource);
+		
+	Employee emp = (Employee) beanFactory.getBean("employee");
+	
+	System.out.println(emp.getName());
+	System.out.println(emp.getMail());
+	System.out.println(emp.getSalary());
+	System.out.println(emp.getAddress());
+	System.out.println();
+	
+	Employee emp1 = (Employee) beanFactory.getBean("employee1");
+	
+	System.out.println(emp1.getName());
+	System.out.println(emp1.getMail());
+	System.out.println(emp1.getSalary());
+	System.out.println(emp1.getAddress());
+	System.out.println();
+	
+	}
+
+}
